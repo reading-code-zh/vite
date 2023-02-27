3a75029604060bd1ddd352009ec7c0b6d24c126d
lei <25927480+yiyi17@users.noreply.github.com>
2023-02-27 22:26:08 +0800

    feat: 1

--- demos/vite-project/vite.config.ts
+++ demos/vite-project/vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer(),
+
+  ],
   cacheDir: './cache',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
