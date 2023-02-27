5f7a9fb4d8583a5a819e842c80231ae1272c07fc
lei <25927480+yiyi17@users.noreply.github.com>
2023-02-27 22:26:22 +0800

    1

--- demos/vite-project/vite.config.ts
+++ demos/vite-project/vite.config.ts
@@ -4,11 +4,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer(),
-
-  ],
+  plugins: [react(), visualizer()],
   cacheDir: './cache',
   build: {
     rollupOptions: {
